3fe222748f98c2091f9f8d3682d1a32213c1e3b5
Diego Betancourt <diferbet@gmail.com>
2022-02-23 19:36:09 -0500

    segundo commit

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -40,3 +40,21 @@ export const setNotes = ( notes ) => ({
     type: types.notesLoad,
     payload: notes
 })
+
+//accion para grabar la nota en la base de datos
+
+export const startSaveNote = (note) => {
+    return async (dispatch, getState) =>{
+        
+        const {uid} = getState().auth;
+
+        if( !note.url ){
+            delete note.url;
+        }
+
+        const noteToFirestore = {...note};
+        delete noteToFirestore.id
+
+        await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
+    }
+}
