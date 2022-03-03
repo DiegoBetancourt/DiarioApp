65f8fb99f2d3966da5dedb1048be8529d1bdd5c8
Diego Betancourt <diferbet@gmail.com>
2022-03-03 09:11:45 -0500

    last commit

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,13 +1,14 @@
-
-import { types } from "../types/types";
+import Swal from "sweetalert2";
 import { db } from "../firebase/firebase-config";
+import { types } from "../types/types";
 import { loadNotes } from "../helpers/loadNotes";
+import { fileUpload } from "../helpers/fileUpload";
 
 
 export const startNewNote = () => {
     return async (dispatch, getState) => {
 
-        const {uid} = getState().auth;
+        const { uid } = getState().auth;
 
         const newNote = {
             title: '',
@@ -15,13 +16,14 @@ export const startNewNote = () => {
             date: new Date().getTime()
         }
 
-        const doc = await db.collection(`${ uid }/journal/notes`).add( newNote);
+        const doc = await db.collection(`${uid}/journal/notes`).add(newNote);
 
-        dispatch( activeNote( doc.id, newNote) )
+        dispatch(activeNote(doc.id, newNote))
+        dispatch(addNewNote(doc.id, newNote));
     }
 }
 
-export const activeNote = ( id, note ) => ({
+export const activeNote = (id, note) => ({
     type: types.notesActive,
     payload: {
         id,
@@ -29,14 +31,22 @@ export const activeNote = ( id, note ) => ({
     }
 })
 
+export const addNewNote = (id, note) => ({
+    type: types.notesAddNew,
+    payload: {
+        id, ...note
+    }
+})
+
+
 export const startLoadingNotes = (uid) => {
-    return async ( dispatch ) => {
+    return async (dispatch) => {
         const notes = await loadNotes(uid)
         dispatch(setNotes(notes))
     }
 }
 
-export const setNotes = ( notes ) => ({
+export const setNotes = (notes) => ({
     type: types.notesLoad,
     payload: notes
 })
@@ -44,17 +54,75 @@ export const setNotes = ( notes ) => ({
 //accion para grabar la nota en la base de datos
 
 export const startSaveNote = (note) => {
-    return async (dispatch, getState) =>{
-        
-        const {uid} = getState().auth;
+    return async (dispatch, getState) => {
 
-        if( !note.url ){
+        const { uid } = getState().auth;
+
+        if (!note.url) {
             delete note.url;
         }
 
-        const noteToFirestore = {...note};
+        const noteToFirestore = { ...note };
         delete noteToFirestore.id
 
         await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
+
+        dispatch(refreshNote(note.id, noteToFirestore));
+
+        Swal.fire('Saved', note.title, 'success');
     }
 }
+
+export const refreshNote = (id, note) => ({
+    type: types.notesUpdated,
+    payload: {
+        id,
+        note: {
+            id,
+            ...note
+        }
+    }
+})
+
+export const startUploading = (file) => {
+    return async (dispatch, getState) => {
+
+        const { active: activeNote } = getState().notes;
+
+        Swal.fire({
+            title: 'Uploading...',
+            text: 'Please wait...',
+            allowOutsideClick: false,
+            onBeforeOpen: () => {
+                Swal.showLoading();
+            }
+        });
+
+        const fileUrl = await fileUpload(file);
+        activeNote.url = fileUrl;
+
+        dispatch(startSaveNote(activeNote))
+
+
+        Swal.close();
+    }
+}
+
+export const startDeleting = (id) => {
+    return async (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        await db.doc(`${uid}/journal/notes/${id}`).delete();
+
+        dispatch(deleteNote(id));
+
+    }
+}
+
+export const deleteNote = (id) => ({
+    type: types.notesDelete,
+    payload: id
+})
+
+export const noteLogout = () => ({
+    type: types.notesLogoutCleaning
+})
